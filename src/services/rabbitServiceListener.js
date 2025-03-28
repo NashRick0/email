373d57956c274b83b516a537ff1d47e8b2c8db33
Mario09122004 <ricardolunaunzueta97@gmail.com>
2025-03-27 19:44:22 -0600

    init email

--- src/services/rabbitServiceListener.js
+++ src/services/rabbitServiceListener.js
@@ -1,47 +1,54 @@
 import amqp from 'amqplib';
 import dotenv from 'dotenv';
-import { sendEmailWelcome } from '../controller/emailController.js'; // Importar la función para enviar el correo de bienvenida
+import { sendEmailWelcome } from '../controller/emailController.js';
 
 dotenv.config();
 
-const RABBITMQ_URL = process.env.RABBITMQ_HOST || 'amqp://admin:admin@rabbitmq';
+const {
+  RABBITMQ_USER,
+  RABBITMQ_PASS,
+  RABBITMQ_HOST,
+  RABBITMQ_VHOST
+} = process.env;
 
-export async function userEvents(){
-    try {
-        const connection = await amqp.connect(RABBITMQ_URL);
-        const channel = await connection.createChannel();
+const RABBITMQ_URL = `amqps://${RABBITMQ_USER}:${RABBITMQ_PASS}@${RABBITMQ_HOST}/${RABBITMQ_VHOST}`;
 
-        const exchange = 'user_event';
-        const queue = 'user_created_queue';
-        const routingKey = 'user.created';
+export async function userEvents() {
+  try {
+    const connection = await amqp.connect(RABBITMQ_URL);
+    const channel = await connection.createChannel();
 
-        await channel.assertExchange(exchange, 'topic', { durable: true });
-        await channel.assertQueue(queue, { durable: true });
-        await channel.bindQueue(queue, exchange, routingKey);
+    const exchange = 'user_event';
+    const queue = 'user_created_queue';
+    const routingKey = 'user.created';
 
-        console.log(`Waiting for messages in ${queue}`);
-        let response = {};
-        channel.consume(queue, (msg) => {
-            if (msg !== null) {
-                response = JSON.parse(msg.content.toString());
-                console.log(response);
+    await channel.assertExchange(exchange, 'topic', { durable: true });
+    await channel.assertQueue(queue, { durable: true });
+    await channel.bindQueue(queue, exchange, routingKey);
 
-                //Enviar correo
-                sendEmailWelcome(response.username);
-                
-                channel.ack(msg);
-            }
-            
-        }, { noAck: false});
+    console.log(`✅ Esperando mensajes en ${queue}`);
+    let response = {};
 
-        connection.on('close', () => {
-            console.error('Conexion cerrada, intentando reconectar en 5 segundos');
-            setTimeout(userEvents, 5000);
-        });
+    channel.consume(queue, (msg) => {
+      if (msg !== null) {
+        response = JSON.parse(msg.content.toString());
+        console.log(response);
 
-    } catch (error) {
-        console.error("Error al conectar con RabbitMQ", error.message);
-        console.error('Intentando reconectar en 5 segundos');
-        setTimeout(userEvents, 5000);
-    }
+        //Enviar correo
+        sendEmailWelcome(response.username);
+
+        channel.ack(msg);
+      }
+    }, { noAck: false });
+
+    connection.on('close', () => {
+      console.error('❌ Conexión cerrada, intentando reconectar en 5 segundos');
+      setTimeout(userEvents, 5000);
+    });
+
+  } catch (error) {
+    console.error('❌ Error al conectar con RabbitMQ:', error.message);
+    console.error('Intentando reconectar en 5 segundos');
+    setTimeout(userEvents, 5000);
+  }
 }
